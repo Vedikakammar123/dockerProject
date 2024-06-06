77c46651d0e3cb2f637645b66d9995bf4c314f1d
Vedikakammar123 <kammarvedika08@gmail.com>
2024-06-06 16:10:44 +0530

    newone

--- my_app/sub_app/urls.py
+++ my_app/sub_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.home,name='home')
+urlpatterns = [
+    path('', views.form_page, name='form_page'),
+    path('thank_you/<int:user_info_id>/', views.thank_you, name='thank_you'),
+    path('show_records/', views.show_records, name='show_records'),
 ]
--- my_app/sub_app/views.py
+++ my_app/sub_app/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render # type: ignore
-from .forms import MyForm 
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from .models import MyModel
 
-# Create your views here.
-def home(request):
-    form=MyForm(request.POST,request.FILES)
-    if request.method=='POST':
-        if form.is_valid():
-            form.save()
-    return render(request,'home.html',{"form":form})
+def form_page(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+       
+        image = request.FILES.get('image')
+
+        if text and  image:
+            user_info = MyModel.objects.create(text=text, image=image)
+            return redirect('thank_you', user_info_id=user_info.id)
+    return render(request, 'home1.html')
+
+def thank_you(request, user_info_id):
+    user_info = MyModel.objects.get(id=user_info_id)
+    return render(request, 'home2.html', {
+        'user_info': user_info,
+        'show_records_url': reverse('show_records')
+    })
+
+def show_records(request):
+    all_user_info = MyModel.objects.all()
+    return render(request, 'show_records.html', {'user_info_list': all_user_info})
